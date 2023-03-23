261d9c45225a370c9faaf1aa97e85b5dfd255c3c
hamisu yusuf <111646226+hamisuyusuf@users.noreply.github.com>
2023-03-23 10:52:17 +0000

    Added job position and API route

--- app.py
+++ app.py
@@ -1,10 +1,42 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__ ,static_url_path='/static')
 
+JOBS = [
+  {
+    'id' : 1,
+    'title': 'Data Scienticst',
+    'location': 'Abuja',
+    'salary': '120,000.000'
+  },
+   {
+    'id' : 2,
+    'title': 'Data Analyst',
+    'location': 'Abuja',
+    'salary': '80,000.000'
+  },
+ {
+    'id' : 3,
+    'title': 'Network Engineer',
+    'location': 'Remote',
+    'salary': '75,000.000'
+  },
+ {
+    'id' : 4,
+    'title': 'Product Designer',
+    'location': 'Abuja',
+    'salary': '70,000.000'
+  }
+]
 
 @app.route("/")
 def hello():
-    return render_template('Home.html')
+    return render_template('Home.html', 
+                           jobs=JOBS,
+                          company_name='nRyde')
+
+@app.route("/api/jobs")
+def list_jobs():
+  return jsonify(JOBS)
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
